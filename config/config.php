0265a529fa93b6bb8960c467d7220b6e5f77230c
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2020-09-28 13:54:21 -0300

    [Feature] Added config params

--- config/config.php
+++ config/config.php
@@ -4,5 +4,15 @@
  * You can place your custom package configuration in here.
  */
 return [
+    // Client ID provided by Mati dashboard
+    'client_id' => env('MATI_CLIENT_ID', null),
 
+    // Client Secret provided by Mati dashboard
+    'client_secret' => env('MATI_CLIENT_SECRET', null),
+
+    // Mati's OAuth URL
+    'api_url' => env('MATI_API_URL', 'https://api.getmati.com/v2'),
+
+    // Mati's API URL
+    'auth_url' => env('MATI_AUTH_URL', 'https://api.getmati.com/oauth')
 ];
