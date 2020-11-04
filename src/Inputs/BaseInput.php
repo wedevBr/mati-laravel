fee9df7e9b52c5c521311ba99aca2867a4204b73
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2020-11-04 10:55:20 -0300

    [Refactor] Renamed IdentityInput to BaseInput

---
+++ src/Inputs/BaseInput.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace WeDevBr\Mati\Inputs;
+
+use WeDevBr\Mati\Support\Contracts\IdentityInputInterface;
+
+/**
+ * Wrapper for identity input
+ *
+ * @author Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
+ */
+abstract class BaseInput implements IdentityInputInterface
+{
+    protected $file_path;
+
+    public function setFilePath(string $path): IdentityInputInterface
+    {
+        $this->file_path = $path;
+        return $this;
+    }
+
+    public function getFileName(): string
+    {
+        return basename($this->file_path);
+    }
+
+    public function getFileContents()
+    {
+        return file_get_contents($this->file_path);
+    }
+
+    abstract public function toArray();
+}
